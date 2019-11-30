fd71c3fdf3648e03b1cc80c08521320d00b371d7
Roel Adriaans <roel@adriaans.org>
2019-11-30 22:06:33 +0100

    Init with base from Advent of code 2018, and first empty solution

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="adventofcode2018",
+    author="Roel Adriaans",
+    author_email="roel@adriaans.org",
+    url="https://github.com/roeladriaans",
+    # version="0.0.2",
+    entry_points={"console_scripts": ["adventofcode = main:main"]},
+    packages=find_packages(where="src", exclude=["tests.*", "test*"]),
+    package_dir={"": "src"},
+    install_requires=requirements,
+    include_package_data=True,
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+)

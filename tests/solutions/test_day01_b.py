e104ef7818604fdd61f5fcde5f1ef5a8b01c4415
Roel Adriaans <roel.adriaans@ordina.nl>
2019-12-01 11:17:37 +0100

    Add day01 solutions

--- tests/solutions/test_day01_b.py
+++ tests/solutions/test_day01_b.py
@@ -4,14 +4,16 @@ from solutions.day01 import Day01PartB
 
 
 class TestDay01PartB:
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day01b_solve(self, input_data, expected_result):
+    @pytest.mark.parametrize(
+        ("input_data", "expected_result"), [(14, 2), (1969, 966), (100756, 50346)],
+    )
+    def test_day01b_solve_module(self, input_data, expected_result):
         solution = Day01PartB()
-        result = solution.solve(input_data)
+        result = solution.compute_including_extra_fuel(input_data)
         assert result == expected_result
 
     def test_day01b_data(self):
         """ Result we got when we did the real solution """
         solution = Day01PartB()
         res = solution("day_01/day01.txt")
-        assert res == 0
+        assert res == 5194970

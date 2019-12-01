a7cee99161cb164451dc19756417d51a43dff41b
Roel Adriaans <roel@adriaans.org>
2019-12-01 11:17:37 +0100

    Add day01 solutions

--- src/solutions/day01.py
+++ src/solutions/day01.py
@@ -2,14 +2,38 @@ from utils.abstract import FileReaderSolution
 
 
 class Day01:
-    pass
+    @staticmethod
+    def compute_fuel(mass):
+        """
+        Fuel required to launch a given module is based on its mass.
+        Specifically, to find the fuel required for a module, take its mass,
+        divide by three, round down, and subtract 2.
+        """
+        fuel = mass // 3 - 2
+        return fuel
+
+    def compute_including_extra_fuel(self, mass):
+        total_fuel = 0
+        extra_fuel = mass
+        # fuel = self.compute_fuel(mass)
+        while True:
+            # Compute the fuel for this fuel
+            extra_fuel = self.compute_fuel(extra_fuel)
+            if extra_fuel <= 0:
+                return total_fuel
+            else:
+                total_fuel += extra_fuel
 
 
 class Day01PartA(Day01, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        fuel = [Day01.compute_fuel(int(mass)) for mass in input_data.splitlines()]
+        return sum(fuel)
 
 
 class Day01PartB(Day01, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        extra_fuel = []
+        for module in input_data.splitlines():
+            extra_fuel.append(self.compute_including_extra_fuel(int(module)))
+        return sum(extra_fuel)
--- tests/solutions/test_day01_a.py
+++ tests/solutions/test_day01_a.py
@@ -4,14 +4,17 @@ from solutions.day01 import Day01PartA
 
 
 class TestDay01PartA:
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
+    @pytest.mark.parametrize(
+        ("input_data", "expected_result"),
+        [(12, 2), (14, 2), (1969, 654), (100756, 33583)],
+    )
     def test_day01a_solve(self, input_data, expected_result):
         solution = Day01PartA()
-        result = solution.solve(input_data)
+        result = solution.compute_fuel(input_data)
         assert result == expected_result
 
     def test_day01a_data(self):
         """ Result we got when we did the real solution """
         solution = Day01PartA()
         res = solution("day_01/day01.txt")
-        assert res == 0
+        assert res == 3465245

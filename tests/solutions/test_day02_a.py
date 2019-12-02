3f9371a6fca73a326bb16fb380c23befc76018ce
Roel Adriaans <roel@road-support.nl>
2019-12-02 06:35:30 +0100

    Add solution Day 2, part a and b

---
+++ tests/solutions/test_day02_a.py
@@ -0,0 +1,27 @@
+import pytest
+
+from solutions.day02 import Day02PartA
+
+
+class TestDay02PartA:
+    @pytest.mark.parametrize(
+        ("input_data", "expected_result"),
+        [
+            ("1,9,10,3,2,3,11,0,99,30,40,50", 3500),
+            ("1,0,0,0,99", 2),
+            ("2,3,0,3,99", 2),
+            ("2,4,4,5,99,0", 2),
+            ("1,1,1,4,99,5,6,0,99", 30),
+        ],
+    )
+    def test_day02a_solve(self, input_data, expected_result):
+        solution = Day02PartA()
+        opcodes = [int(digit) for digit in input_data.split(",")]
+        result = solution.look_at_opcodes(opcodes)[0]
+        assert result == expected_result
+
+    def test_day02a_data(self):
+        """ Result we got when we did the real solution """
+        solution = Day02PartA()
+        res = solution("day_02/day02.txt")
+        assert res == 3306701
---
+++ tests/solutions/test_day02_b.py
@@ -0,0 +1,9 @@
+from solutions.day02 import Day02PartB
+
+
+class TestDay02PartB:
+    def test_day02b_data(self):
+        """ Result we got when we did the real solution """
+        solution = Day02PartB()
+        res = solution("day_02/day02.txt")
+        assert res == 7621

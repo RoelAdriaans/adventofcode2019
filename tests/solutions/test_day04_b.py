9fb289ab9d9387bce318e7d05638494be503c03f
Roel Adriaans <roel@road-support.nl>
2019-12-04 06:44:31 +0100

    WIP Day 04 part B, no time left to create solution

--- tests/solutions/test_day04_b.py
+++ tests/solutions/test_day04_b.py
@@ -4,14 +4,29 @@ from solutions.day04 import Day04PartB
 
 
 class TestDay04PartB:
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
+    @pytest.mark.parametrize(
+        ("input_data", "expected_result"),
+        [
+            (112233, True),
+            (123444, False),
+            (111122, True),
+            (111111, False),  # Probably false now?
+            (223450, False),
+            (123789, False),
+            (1234, False),
+        ],
+    )
     def test_day04b_solve(self, input_data, expected_result):
         solution = Day04PartB()
-        result = solution.solve(input_data)
+        result = solution.is_valid_part_b(input_data)
         assert result == expected_result
 
     def test_day04b_data(self):
         """ Result we got when we did the real solution """
         solution = Day04PartB()
         res = solution("day_04/day04.txt")
+        assert res != 74
+        assert res != 583
+        assert res != 453
+        assert res != 70
         assert res == 0

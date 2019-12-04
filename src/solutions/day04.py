c2baa2479ea40a79f6e98720b9b7496fef0729e7
Roel Adriaans <roel@adriaans.org>
2019-12-04 06:44:31 +0100

    WIP Day 04 part B, no time left to create solution

--- src/solutions/day04.py
+++ src/solutions/day04.py
@@ -1,5 +1,5 @@
 from utils.abstract import FileReaderSolution
-
+import re
 
 class Day04:
     @staticmethod
@@ -29,6 +29,40 @@ class Day04:
 
         return True
 
+    @staticmethod
+    def is_valid_part_b(int_input: int) -> bool:
+        str_input = str(int_input)
+        if len(str_input) != 6:
+            return False
+
+        two_consecetive = False
+        digit_in = False
+
+        for digit in range(0, 10):
+            str_digit = str(digit)
+            replaced = str_input.replace(f"{str_digit}{str_digit}", "")
+            if len(replaced) == 4 :
+                two_consecetive = True
+            if str_digit in replaced:
+                # Still a digit in there
+                digit_in = True
+
+        if not two_consecetive:
+            return False
+
+        if digit_in:
+            return False
+
+        max_found = float("-inf")
+
+        for digit in str_input:
+            if int(digit) >= max_found:
+                max_found = int(digit)
+            else:
+                return False
+
+        return True
+
 
 class Day04PartA(Day04, FileReaderSolution):
     def solve(self, input_data: str) -> int:
@@ -36,7 +70,7 @@ class Day04PartA(Day04, FileReaderSolution):
         lower = int(lower)
         uper = int(uper)
         counter = 0
-        for x in range(lower, uper+1):
+        for x in range(lower, uper + 1):
             valid = self.is_valid(x)
             if valid:
                 counter += 1
@@ -45,4 +79,14 @@ class Day04PartA(Day04, FileReaderSolution):
 
 class Day04PartB(Day04, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+
+        lower, uper = input_data.split("-")
+        lower = int(lower)
+        uper = int(uper)
+        counter = 0
+        for x in range(lower, uper + 1):
+            valid = self.is_valid_part_b(x)
+            if valid:
+                counter += 1
+        return counter
+

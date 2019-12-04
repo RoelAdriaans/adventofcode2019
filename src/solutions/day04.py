ab8c33e1bae7786b97f545d06f99b4cd2b099c82
Roel Adriaans <roel.adriaans01@rabobank.com>
2019-12-04 08:59:35 +0100

    Extract tests to seperate functions

--- src/solutions/day04.py
+++ src/solutions/day04.py
@@ -1,33 +1,50 @@
 from utils.abstract import FileReaderSolution
 import re
 
+
 class Day04:
     @staticmethod
-    def is_valid(int_input: int) -> bool:
-        str_input = str(int_input)
-        if len(str_input) != 6:
-            return False
+    def is_valid_length(number_str: str) -> bool:
+        """Is this string exactly 6 characters"""
+        # Technically, the string will always be 6 long, we define that input.
+        # Keeping the test anyway, it makes this rule more implicit.
 
-        two_consecetive = False
+        return len(number_str) == 6
+
+    @staticmethod
+    def is_consecutive(number_str: str) -> bool:
         for digit in range(0, 10):
             str_digit = str(digit)
-            replaced = str_input.replace(f"{str_digit}{str_digit}", "")
+            replaced = number_str.replace(f"{str_digit}{str_digit}", "")
             if len(replaced) <= 5:
-                two_consecetive = True
-                break
+                return True
+        return False
 
-        if not two_consecetive:
-            return False
-
+    @staticmethod
+    def is_going_up(number_str: str) -> bool:
+        # Validate that the numbers go up or are the same.
         max_found = float("-inf")
 
-        for digit in str_input:
+        for digit in number_str:
             if int(digit) >= max_found:
                 max_found = int(digit)
             else:
                 return False
-
         return True
+
+    @staticmethod
+    def is_valid_part_a(int_input: int) -> bool:
+        str_input = str(int_input)
+        if all(
+            [
+                Day04.is_valid_length(str_input),
+                Day04.is_consecutive(str_input),
+                Day04.is_going_up(str_input),
+            ]
+        ):
+            return True
+
+        return False
 
     @staticmethod
     def is_valid_part_b(int_input: int) -> bool:
@@ -41,7 +58,7 @@ class Day04:
         for digit in range(0, 10):
             str_digit = str(digit)
             replaced = str_input.replace(f"{str_digit}{str_digit}", "")
-            if len(replaced) == 4 :
+            if len(replaced) == 4:
                 two_consecetive = True
             if str_digit in replaced:
                 # Still a digit in there
@@ -71,7 +88,7 @@ class Day04PartA(Day04, FileReaderSolution):
         uper = int(uper)
         counter = 0
         for x in range(lower, uper + 1):
-            valid = self.is_valid(x)
+            valid = self.is_valid_part_a(x)
             if valid:
                 counter += 1
         return counter
@@ -89,4 +106,3 @@ class Day04PartB(Day04, FileReaderSolution):
             if valid:
                 counter += 1
         return counter
-
--- tests/solutions/test_day04_a.py
+++ tests/solutions/test_day04_a.py
@@ -6,12 +6,11 @@ from solutions.day04 import Day04PartA
 class TestDay04PartA:
     @pytest.mark.parametrize(
         ("input_data", "expected_result"),
-        [(111111, True), (223450, False), (123789, False), (1234, False)
-         ],
+        [(111111, True), (223450, False), (123789, False), (1234, False)],
     )
     def test_day04a_solve(self, input_data, expected_result):
         solution = Day04PartA()
-        result = solution.is_valid(input_data)
+        result = solution.is_valid_part_a(input_data)
         assert result == expected_result
 
     def test_day04a_data(self):
@@ -20,4 +19,3 @@ class TestDay04PartA:
         res = solution("day_04/day04.txt")
         assert res != 246666
         assert res == 1048
-

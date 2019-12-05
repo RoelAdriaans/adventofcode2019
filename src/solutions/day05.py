707410227848e351b7fe81b52664f35a764f9f5a
Roel Adriaans <roel@road-support.nl>
2019-12-05 21:53:14 +0100

    Add working day 5

--- src/solutions/day05.py
+++ src/solutions/day05.py
@@ -2,6 +2,10 @@ from utils.abstract import FileReaderSolution
 from typing import List
 
 
+class ProgramFinished(Exception):
+    pass
+
+
 class Day05:
     program_counter = 0
     instructions = []
@@ -10,64 +14,110 @@ class Day05:
         self.program_counter = 0
         self.instructions = instructions[:]
 
-    def _get_location(self):
+    def _parse_immediate_mode(self):
         current_opcode = self.instructions[self.program_counter]
-        str_opcode = str(current_opcode)
 
-        if len(str_opcode) >= 4:
-            # We are in Parameter modes
-            current_opcode = int(str_opcode[-2:])
-            param_1_position_mode = bool(str_opcode[len(str_opcode) - 3])
-            param_2_position_mode = bool(str_opcode[len(str_opcode) - 4])
-            if len(str_opcode) == 5:
-                param_3_position_mode = bool(str_opcode[len(str_opcode) - 5])
-            else:
-                param_3_position_mode = None
-                param_is
+        # Let's zeropad the string to make it easier to process
+        str_opcode = f"{current_opcode:05}"
+        # We are in Parameter modes
+        current_opcode = int(str_opcode[-2:])
+        param_1_position_mode = not bool(int(str_opcode[len(str_opcode) - 3]))
+        param_2_position_mode = not bool(int(str_opcode[len(str_opcode) - 4]))
+        param_3_position_mode = not bool(int(str_opcode[len(str_opcode) - 5]))
 
+        return (
+            current_opcode,
+            param_1_position_mode,
+            param_2_position_mode,
+            param_3_position_mode,
+        )
 
-    def process_instruction(self):
+    def _get_instruction(self, position_mode: bool, value) -> int:
+        """
+        If `position_mode` is True, return the value in that position
+        If `position_mode` is False,
+        """
+        if position_mode:
+            return self.instructions[value]
+        else:
+            return value
+
+    def process_instruction(self, input_value=None):
         """ Process the current instruction and increate the program counter"""
         current_opcode = self.instructions[self.program_counter]
         str_opcode = str(current_opcode)
+        if len(str_opcode) >= 3:
+            (
+                current_opcode,
+                param_1_position_mode,
+                param_2_position_mode,
+                param_3_position_mode,
+            ) = self._parse_immediate_mode()
+        else:
+            # No long upcode, these will be false
+            param_1_position_mode = True
+            param_2_position_mode = True
+            param_3_position_mode = True
 
         if current_opcode == 99:
-            return
+            raise ProgramFinished
 
         if current_opcode == 1:
-            pos_1 = self.instructions[self.program_counter + 1]
-            pos_2 = self.instructions[self.program_counter + 2]
+            val_1 = self._get_instruction(
+                param_1_position_mode, self.instructions[self.program_counter + 1]
+            )
+            val_2 = self._get_instruction(
+                param_2_position_mode, self.instructions[self.program_counter + 2]
+            )
             store = self.instructions[self.program_counter + 3]
 
-            add = self.instructions[pos_1] + self.instructions[pos_2]
-            self.instructions[store] = add
+            self.instructions[store] = val_1 + val_2
             self.program_counter += 4
 
         elif current_opcode == 2:
-            pos_1 = self.instructions[self.program_counter + 1]
-            pos_2 = self.instructions[self.program_counter + 2]
+            val_1 = self._get_instruction(
+                param_1_position_mode, self.instructions[self.program_counter + 1]
+            )
+            val_2 = self._get_instruction(
+                param_2_position_mode, self.instructions[self.program_counter + 2]
+            )
+
             store = self.instructions[self.program_counter + 3]
 
-            mult = self.instructions[pos_1] * self.instructions[pos_2]
-            self.instructions[store] = mult
+            self.instructions[store] = val_1 * val_2
             self.program_counter += 4
         elif current_opcode == 3:
-            input_value = 1
+            # Use input
             store = self.instructions[self.program_counter + 1]
-
             self.instructions[store] = input_value
-
-        elif current_opcode == 4:
-            output = self.instructions[self.program_counter + 1]
             self.program_counter += 2
-            print(output)
+        elif current_opcode == 4:
+            # Return output
+            output = self._get_instruction(
+                param_1_position_mode, self.instructions[self.program_counter + 1]
+            )
+
+            self.program_counter += 2
+            return output
         else:
-            raise ValueError("Unknown opcode")
+            raise ValueError(f"Unknown opcode: {current_opcode}")
 
 
 class Day05PartA(Day05, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        # Technically not 100% correct, when res is non zero, if should check that the
+        # next instructions terminates the program
+        instructions = list(map(int, input_data.split(",")))
+        self.load_instructions(instructions)
+        res = False
+        try:
+            while True:
+                res = self.process_instruction(1)
+                if res not in (0, None):
+                    return res
+        except ProgramFinished:
+            pass
+        return res
 
 
 class Day05PartB(Day05, FileReaderSolution):

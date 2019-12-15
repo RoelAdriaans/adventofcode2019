6b658631c3e10d0a59e9d123f65648fc02c4746b
Roel Adriaans <roel.adriaans@ordina.nl>
2019-12-15 20:13:53 +0100

    Solution for day 7 b

--- src/solutions/day07.py
+++ src/solutions/day07.py
@@ -1,5 +1,5 @@
 from utils.abstract import FileReaderSolution
-from solutions.intcode import IntCode
+from solutions.intcode import IntCode, ProgramFinished
 from typing import List, Tuple, Union
 import itertools
 
@@ -38,17 +38,73 @@ class Day07:
                 max_result = result
         return "".join(map(str, max_sequence)), max_result
 
+    def compute_results_for_looped_sequence(
+        self, instructions: List[int], sequence: Tuple
+    ) -> int:
+        """
+        For program `instructions`, compute the result for the sequence in the string
+        `sequence`, eg "01234"
+        """
+        amplifiers = []
+        # First we create the 5 amplifiers
+        for amplifier_number in sequence:
+            amplifier = IntCode()
+            amplifier.done = False
+            amplifier.load_instructions(instructions)
+            amplifier.load_input_values([amplifier_number])
+            amplifiers.append(amplifier)
+
+        # And now, run
+        last_value = None
+        num_done = 0
+        # Run the sequence:
+        result = 0
+        while True:
+            for amplifier in amplifiers:
+                if amplifier.done:
+                    continue
+                amplifier.load_input_values([result])
+                try:
+                    result = amplifier.run_return_or_raise()
+
+                except ProgramFinished:
+                    # Program is done!
+                    amplifier.done = True
+                    num_done += 1
+                    if num_done == 5:
+                        return last_value
+
+            # We have done a loop, let's check if this is the bigest value
+            if result is None:
+                return last_value
+            else:
+                last_value = result
+
+    def get_best_looped_sequence(
+        self, instructions: List[int]
+    ) -> Tuple[str, Union[float, int]]:
+        """ Get the sequence with the best outpu t"""
+        # Generate all the options
+        # We loop because we have 10 computers,
+        max_result = float("-inf")
+        max_sequence = ""
+        for sequence in itertools.permutations(range(5, 10)):
+            result = self.compute_results_for_looped_sequence(instructions, sequence)
+            if result > max_result:
+                max_sequence = sequence
+                max_result = result
+        return "".join(map(str, max_sequence)), max_result
+
 
 class Day07PartA(Day07, FileReaderSolution):
     def solve(self, input_data: str) -> int:
         instructions = list(map(int, input_data.split(",")))
-        # computer = IntCode()
-        # computer.load_instructions(instructions)
         sequence, thrust = self.get_best_sequence(instructions)
         return thrust
 
 
-
 class Day07PartB(Day07, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        instructions = list(map(int, input_data.split(",")))
+        sequence, thrust = self.get_best_looped_sequence(instructions)
+        return thrust
--- tests/solutions/test_day07_b.py
+++ tests/solutions/test_day07_b.py
@@ -4,11 +4,30 @@ from solutions.day07 import Day07PartB
 
 
 class TestDay07PartB:
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day07b_solve(self, input_data, expected_result):
+    @pytest.mark.parametrize(
+        ("instructions", "expected_sequence", "expected_thrust"),
+        [
+            (
+                "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,"
+                "27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5",
+                "98765",
+                139629729,
+            ),
+            (
+                "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,"
+                "-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,"
+                "53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10",
+                "97856",
+                18216,
+            ),
+        ],
+    )
+    def test_day07b_solve(self, instructions, expected_sequence, expected_thrust):
+        instructions = list(map(int, instructions.split(",")))
         solution = Day07PartB()
-        result = solution.solve(input_data)
-        assert result == expected_result
+        sequence, thrust = solution.get_best_looped_sequence(instructions)
+        assert sequence == expected_sequence
+        assert thrust == expected_thrust
 
     def test_day07b_data(self):
         """ Result we got when we did the real solution """

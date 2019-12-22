52639ebb8a3f059ade611ee42782d53ef11d38c0
Roel Adriaans <roel@road-support.nl>
2019-12-22 11:40:17 +0100

    Run Day9 B

--- src/solutions/day09.py
+++ src/solutions/day09.py
@@ -3,20 +3,21 @@ from solutions.intcode import IntCode
 
 
 class Day09:
-    pass
-
-
-class Day09PartA(Day09, FileReaderSolution):
-    def solve(self, input_data: str) -> int:
+    def run_computer(self, input_data: str, input_value: int) -> int:
         instructions = list(map(int, input_data.split(",")))
         intcode = IntCode()
         intcode.load_instructions(instructions)
-        intcode.load_input_values([1])
+        intcode.load_input_values([input_value])
 
         result = intcode.run_until_finished()
         return result[0]
 
 
+class Day09PartA(Day09, FileReaderSolution):
+    def solve(self, input_data: str) -> int:
+        return self.run_computer(input_data, 1)
+
+
 class Day09PartB(Day09, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        return 0
+        return self.run_computer(input_data, 2)

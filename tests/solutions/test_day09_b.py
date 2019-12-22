b2904885451d11cac8305a145f67122fc807a7e6
Roel Adriaans <roel@adriaans.org>
2019-12-22 11:40:17 +0100

    Run Day9 B

--- tests/solutions/test_day09_b.py
+++ tests/solutions/test_day09_b.py
@@ -1,19 +1,9 @@
-import pytest
-
 from solutions.day09 import Day09PartB
 
 
 class TestDay09PartB:
-    @pytest.skip("Not yet implemented")
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day09b_solve(self, input_data, expected_result):
-        solution = Day09PartB()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.skip("Not yet implemented")
     def test_day09b_data(self):
         """ Result we got when we did the real solution """
         solution = Day09PartB()
         res = solution("day_09/day09.txt")
-        assert res == 0
+        assert res == 78831

d02dd2fe6197413f7eaa5145ec6d5f94895bc06f
Roel Adriaans <roel@adriaans.org>
2019-12-24 12:41:45 +0100

    Add Day 10 part B.

--- tests/solutions/test_day10_b.py
+++ tests/solutions/test_day10_b.py
@@ -24,11 +24,9 @@ class TestDay10PartB:
         # We start going up:
         assert solution.laser_direction == 90
 
-        first_one = solution.rotate_and_shoot_until_hit(best_spot)
-        assert first_one == Astroid(x=8, y=1)
-
-        shot = solution.rotate_and_shoot_until_hit(best_spot)
-        assert shot == Astroid(x=9, y=0)
+        solution.remove_astroid(best_spot, n=3)
+        assert solution.astroids_destroyed[0] == Astroid(x=8, y=1)
+        assert solution.astroids_destroyed[1] == Astroid(x=9, y=0)
 
     def test_distance(self):
         # Problem wit distance during the test:
@@ -39,7 +37,7 @@ class TestDay10PartB:
         distance_1 = Day10PartB._get_distance(location, astroid_1)
         distance_2 = Day10PartB._get_distance(location, astroid_2)
 
-        assert distance_1 < distance_2
+        assert distance_1 > distance_2
 
     def test_day10b_big_map(self):
         input_map = [
@@ -70,23 +68,18 @@ class TestDay10PartB:
         best_astroid, num_hits = solution.get_best_astroid()
         assert num_hits == 210
         assert best_astroid == Astroid(11, 13)
-        solution.shoot_and_rotate(best_astroid, n=20)
+        solution.remove_astroid(best_astroid, n=200)
 
         assert solution.astroids_destroyed[0] == Astroid(11, 12)  # 1th
         assert solution.astroids_destroyed[1] == Astroid(12, 1)  # 2 th
         assert solution.astroids_destroyed[2] == Astroid(12, 2)  # 3 th
-        assert solution.astroids_destroyed[9] == Astroid(12, 2)  # 10th
+        assert solution.astroids_destroyed[9] == Astroid(12, 8)  # 10th
+        assert solution.astroids_destroyed[19] == Astroid(16, 0)  # 20th
+        assert solution.astroids_destroyed[49] == Astroid(16, 9)  # 50th
+        assert solution.astroids_destroyed[199] == Astroid(8, 2)  # 200th
 
-    @pytest.mark.skip("Not get NotImplemented")
-    @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
-    def test_day10b_solve(self, input_data, expected_result):
-        solution = Day10PartB()
-        result = solution.solve(input_data)
-        assert result == expected_result
-
-    @pytest.mark.skip("Not get NotImplemented")
     def test_day10b_data(self):
         """ Result we got when we did the real solution """
         solution = Day10PartB()
         res = solution("day_10/day10.txt")
-        assert res == 0
+        assert res == 517

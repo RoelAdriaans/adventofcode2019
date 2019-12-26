decb7554aec9c82be59da1222ac577fdee979abf
Roel Adriaans <roel@road-support.nl>
2019-12-26 13:19:35 +0100

    Skip new tests by default, make tox work when solutions are not yet finished

--- template/{{cookiecutter.advendofcode2019}}/tests/solutions/test_{{cookiecutter.file_name}}_a.py
+++ template/{{cookiecutter.advendofcode2019}}/tests/solutions/test_{{cookiecutter.file_name}}_a.py
@@ -4,12 +4,14 @@ from solutions.{{cookiecutter.file_name}} import {{cookiecutter.class_name}}Part
 
 
 class Test{{cookiecutter.class_name}}PartA:
+    @pytest.mark.skip("This code is not yet implemented.")
     @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
     def test_{{cookiecutter.file_name}}a_solve(self, input_data, expected_result):
         solution = {{cookiecutter.class_name}}PartA()
         result = solution.solve(input_data)
         assert result == expected_result
 
+    @pytest.mark.skip("This code is not yet implemented.")
     def test_{{cookiecutter.file_name}}a_data(self):
         """ Result we got when we did the real solution """
         solution = {{cookiecutter.class_name}}PartA()

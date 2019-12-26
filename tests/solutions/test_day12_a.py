a1b0ae869f2f18e6ae3fc55bd2ca25b462aed226
Roel Adriaans <roel@adriaans.org>
2019-12-26 15:55:45 +0100

    Brute force solution for Day12, but this will run until the end of the universe

--- tests/solutions/test_day12_a.py
+++ tests/solutions/test_day12_a.py
@@ -1,6 +1,4 @@
-import pytest
-
-from solutions.day12 import Day12PartA, Moon
+from solutions.day12 import Day12PartA, Moon, Galaxy
 
 
 class TestDay12PartA:
@@ -21,16 +19,16 @@ class TestDay12PartA:
             "<x=3, y=5, z=-1>",
         ]
         input_string = "\n".join(input_strings)
-        solution = Day12PartA()
-        solution.create_moons(input_string)
+        galaxy = Galaxy()
+        galaxy.create_moons(input_string)
 
-        assert len(solution.moons) == 4
+        assert len(galaxy.moons) == 4
         assert (
-            repr(solution.moons[0])
+            repr(galaxy.moons[0])
             == "pos=<x= -1, y=  0, z=  2>, vel=<x=  0, y=  0, z=  0>"
         )
         # Let's assume the rest is the same and that creating the Moons worked
-        solution.step()
+        galaxy.step()
 
         step_1_pos = [
             "pos=<x=  2, y= -1, z=  1>, vel=<x=  3, y= -1, z= -1>",
@@ -39,10 +37,10 @@ class TestDay12PartA:
             "pos=<x=  2, y=  2, z=  0>, vel=<x= -1, y= -3, z=  1>",
         ]
         for i, expected_position in enumerate(step_1_pos):
-            assert repr(solution.moons[i]) == expected_position
+            assert repr(galaxy.moons[i]) == expected_position
 
         # Step another 9 times to get to step 10:
-        solution.step_multi(9)
+        galaxy.step_multi(9)
         step_10_pos = [
             "pos=<x=  2, y=  1, z= -3>, vel=<x= -3, y= -2, z=  1>",
             "pos=<x=  1, y= -8, z=  0>, vel=<x= -1, y=  1, z=  3>",
@@ -50,9 +48,9 @@ class TestDay12PartA:
             "pos=<x=  2, y=  0, z=  4>, vel=<x=  1, y= -1, z= -1>",
         ]
         for i, expected_position in enumerate(step_10_pos):
-            assert repr(solution.moons[i]) == expected_position
+            assert repr(galaxy.moons[i]) == expected_position
 
-        assert solution.get_total_energy() == 179
+        assert galaxy.get_total_energy() == 179
 
     def test_day12a_data(self):
         """ Result we got when we did the real solution """

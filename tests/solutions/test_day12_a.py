0938ab3e6789f7002355c908223700e470c6171d
Roel Adriaans <roel@road-support.nl>
2019-12-26 15:39:08 +0100

    Add Day 12, part A

---
+++ tests/solutions/test_day12_a.py
@@ -0,0 +1,61 @@
+import pytest
+
+from solutions.day12 import Day12PartA, Moon
+
+
+class TestDay12PartA:
+    def test_moon(self):
+        input_string = "<x=-1, y=0, z=2>"
+        moon = Moon.parse_input(input_string)
+        assert moon.x == -1
+        assert moon.y == 0
+        assert moon.z == 2
+
+        assert repr(moon) == "pos=<x= -1, y=  0, z=  2>, vel=<x=  0, y=  0, z=  0>"
+
+    def test_creation(self):
+        input_strings = [
+            "<x=-1, y=0, z=2>",
+            "<x=2, y=-10, z=-7>",
+            "<x=4, y=-8, z=8>",
+            "<x=3, y=5, z=-1>",
+        ]
+        input_string = "\n".join(input_strings)
+        solution = Day12PartA()
+        solution.create_moons(input_string)
+
+        assert len(solution.moons) == 4
+        assert (
+            repr(solution.moons[0])
+            == "pos=<x= -1, y=  0, z=  2>, vel=<x=  0, y=  0, z=  0>"
+        )
+        # Let's assume the rest is the same and that creating the Moons worked
+        solution.step()
+
+        step_1_pos = [
+            "pos=<x=  2, y= -1, z=  1>, vel=<x=  3, y= -1, z= -1>",
+            "pos=<x=  3, y= -7, z= -4>, vel=<x=  1, y=  3, z=  3>",
+            "pos=<x=  1, y= -7, z=  5>, vel=<x= -3, y=  1, z= -3>",
+            "pos=<x=  2, y=  2, z=  0>, vel=<x= -1, y= -3, z=  1>",
+        ]
+        for i, expected_position in enumerate(step_1_pos):
+            assert repr(solution.moons[i]) == expected_position
+
+        # Step another 9 times to get to step 10:
+        solution.step_multi(9)
+        step_10_pos = [
+            "pos=<x=  2, y=  1, z= -3>, vel=<x= -3, y= -2, z=  1>",
+            "pos=<x=  1, y= -8, z=  0>, vel=<x= -1, y=  1, z=  3>",
+            "pos=<x=  3, y= -6, z=  1>, vel=<x=  3, y=  2, z= -3>",
+            "pos=<x=  2, y=  0, z=  4>, vel=<x=  1, y= -1, z= -1>",
+        ]
+        for i, expected_position in enumerate(step_10_pos):
+            assert repr(solution.moons[i]) == expected_position
+
+        assert solution.get_total_energy() == 179
+
+    def test_day12a_data(self):
+        """ Result we got when we did the real solution """
+        solution = Day12PartA()
+        res = solution("day_12/day12.txt")
+        assert res == 5350

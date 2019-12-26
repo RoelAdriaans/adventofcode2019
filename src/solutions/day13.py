e6ea5c314ce84248309df2711952fb43f0eee42f
Roel Adriaans <roel@adriaans.org>
2019-12-26 20:10:30 +0100

    Add solution for Day 13 B
    Total time for Part A and B was 45 minutes, inclusing distractions
    from Spook. This was fun :)

--- src/solutions/day13.py
+++ src/solutions/day13.py
@@ -3,6 +3,8 @@ from solutions.intcode import IntCode, ProgramFinished
 from utils.advent_utils import string_to_list_of_ints
 from enum import IntEnum
 from collections import defaultdict
+from typing import List
+
 
 class Tile(IntEnum):
     EMPTY = 0
@@ -11,43 +13,107 @@ class Tile(IntEnum):
     PADDLE = 3
     BALL = 4
 
+    @staticmethod
+    def show(tile):
+        if tile == Tile.EMPTY:
+            return " "
+        elif tile == Tile.WALL:
+            return "#"
+        elif tile == Tile.BLOCK:
+            return "*"
+        elif tile == Tile.PADDLE:
+            return "-"
+        elif tile == Tile.BALL:
+            return "O"
+
 
 class Day13:
     screen: defaultdict
+
+    def show_screen(self):
+        """
+        Draw the screen on the console:
+        - Empty : " "
+        - Wall : "#"
+        - Block : "*"
+        - Paddle : "-"
+        - Ball : "O"
+
+        Size of the screen is:
+        max x: 37
+        max y: 19
+        """
+        max_y = max([point[1] for point in self.screen.keys()])
+        max_x = max([point[0] for point in self.screen.keys()])
+        grid = []
+        for y in range(0, max_y):
+            row = []
+            for x in range(0, max_x):
+                row.append(Tile.show(self.screen[x, y]))
+            grid.append("".join(row))
+        return "\n".join(grid)
 
     def draw_tile(self, x: int, y: int, tile: Tile) -> None:
         """ Draw a single tile """
         self.screen[(x, y)] = tile
 
-    def run_arcade(self, input_data: str) -> int:
-        self.screen = defaultdict(int)
+    def get_location_for_tiles(self, type: Tile) -> List:
+        tiles = [location for location, value in self.screen.items() if value == type]
+        return tiles
 
+    def run_arcade(self, input_data: str, free_to_play=False) -> int:
+        self.screen = defaultdict(int)
+        score = 0
         instructions = string_to_list_of_ints(input_data)
+        if free_to_play:
+            # Is we allow free to play, Set instruction 0 to 2
+            instructions[0] = 2
 
         intcode = IntCode()
         intcode.load_instructions(instructions)
+        # Let's load the first instruction
+        intcode.load_input_values([0])
         try:
             while True:
                 x = intcode.run_return_or_raise()
                 y = intcode.run_return_or_raise()
                 tile = intcode.run_return_or_raise()
-                tile_obj = Tile(tile)
-                self.draw_tile(x, y, tile_obj)
+                if x == -1 and y == 0:
+                    score = tile
+                else:
+                    tile_obj = Tile(tile)
+                    self.draw_tile(x, y, tile_obj)
+                # if free_to_play:
+                #     print(self.show_screen())
+
+                # Compute there the ball is, and put the joystick to the right side
+                ball = self.get_location_for_tiles(Tile.BALL)
+                paddle = self.get_location_for_tiles(Tile.PADDLE)
+                if ball and paddle:
+                    # Check that we have the grid ready
+                    if ball[0][0] < paddle[0][0]:
+                        direction = -1
+                    elif ball[0][0] > paddle[0][0]:
+                        direction = 1
+                    else:
+                        direction = 0
+                else:
+                    direction = 0
+
+                intcode.set_input_value([direction])
 
         except ProgramFinished:
-            return 0
+            return score
 
 
 class Day13PartA(Day13, FileReaderSolution):
     def solve(self, input_data: str) -> int:
         self.run_arcade(input_data)
 
-        block_tiles = [
-            location for location, value in self.screen.items() if value == Tile.BLOCK
-        ]
+        block_tiles = self.get_location_for_tiles(Tile.BLOCK)
         return len(block_tiles)
 
 
 class Day13PartB(Day13, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        return self.run_arcade(input_data, free_to_play=True)
---
+++ tests/solutions/test_day13_b.py
@@ -0,0 +1,11 @@
+import pytest
+
+from solutions.day13 import Day13PartB
+
+
+class TestDay13PartB:
+    def test_day13b_data(self):
+        """ Result we got when we did the real solution """
+        solution = Day13PartB()
+        res = solution("day_13/day13.txt")
+        assert res == 10292

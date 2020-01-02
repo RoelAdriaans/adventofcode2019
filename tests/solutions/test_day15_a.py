c862569225542f82153e05dccbcd8cd9b01e7c9b
Roel Adriaans <roel.adriaans@ordina.nl>
2020-01-02 22:01:57 +0100

    Return the right solution, but with copied code. Time to fix it

--- tests/solutions/test_day15_a.py
+++ tests/solutions/test_day15_a.py
@@ -1,6 +1,6 @@
 import pytest
 
-from solutions.day15 import Day15PartA, Movement
+from solutions.day15 import Day15PartA, Movement, Point, Location
 
 
 class TestDay15PartA:
@@ -31,6 +31,14 @@ class TestDay15PartA:
         movement = Movement.rotate(movement, 0)
         assert movement.value == Movement.NORTH
 
+    def test_location(self):
+        p1 = Point(1, 0)
+        p2 = Point(2, 0)
+        l1 = Location(point=p1)
+        l2 = Location(point=p2)
+        l1.set_edge(Movement.NORTH, l2)
+        assert repr(l1) == "(Point(x=1, y=0) -   - edges: [Point(x=2, y=0)])"
+
     @pytest.mark.skip("This code is not yet implemented.")
     @pytest.mark.parametrize(("input_data", "expected_result"), [("", ""), ("", "")])
     def test_day15a_solve(self, input_data, expected_result):
@@ -42,4 +50,4 @@ class TestDay15PartA:
         """ Result we got when we did the real solution """
         solution = Day15PartA()
         res = solution("day_15/day15.txt")
-        assert res == 0
+        assert res == 228

efa9389b86e13254e03ad2133074c9704ff3b83c
Roel Adriaans <roel.adriaans@ordina.nl>
2020-01-03 13:10:13 +0100

    Much cleaner solution for Day 15A

--- src/solutions/day15.py
+++ src/solutions/day15.py
@@ -14,35 +14,14 @@ class Point(NamedTuple):
     y: int
 
 
-class Rotate(IntEnum):
-    LEFT = 0
-    RIGHT = 1
-
-
-class Movement(IntEnum):
+class Direction(IntEnum):
     NORTH = 1
     SOUTH = 2
     WEST = 3
     EAST = 4
 
     @staticmethod
-    def rotate(current_direction: "Movement", rotate_direction: Rotate) -> "Movement":
-        """ Rotate the robot
-        :param current_direction: Instance of Movement with current durection
-        :param rotate_direction: Rotate: 0 Rotate left, 1 Rotate right
-        """
-        if rotate_direction == Rotate.RIGHT:
-            turn_dict = {1: 4, 2: 3, 3: 1, 4: 2}
-        elif rotate_direction == Rotate.LEFT:
-            turn_dict = {1: 3, 2: 4, 3: 2, 4: 1}
-        else:
-            raise ValueError(f"Unknown rotateion {rotate_direction=}")
-
-        new_direction = turn_dict[current_direction.value]
-        return Movement(new_direction)
-
-    @staticmethod
-    def direction_update_x_y(current_direction: "Movement", point: Point) -> Point:
+    def direction_update_x_y(current_direction: "Direction", point: Point) -> Point:
         """
         Update the coordinate if the movement succeeded
 
@@ -50,42 +29,22 @@ class Movement(IntEnum):
         :param point: Current position
         :return: Point) with new direction
         """
-        if current_direction == Movement.NORTH:
+        if current_direction == Direction.NORTH:
             return Point(point.x, point.y - 1)
-        elif current_direction == Movement.SOUTH:
+        elif current_direction == Direction.SOUTH:
             return Point(point.x, point.y + 1)
-        elif current_direction == Movement.EAST:
+        elif current_direction == Direction.EAST:
             return Point(point.x + 1, point.y)
-        elif current_direction == Movement.WEST:
+        elif current_direction == Direction.WEST:
             return Point(point.x - 1, point.y)
         else:
             raise ValueError(f"Invalid current direction {current_direction}")
-
-    @staticmethod
-    def get_opposite_direction(current_direction: "Movement"):
-        """ Get the opposite direction
-
-        North returns south, West returns east, ...
-        """
-        location_dict = {
-            Movement.NORTH: Movement.SOUTH,
-            Movement.SOUTH: Movement.NORTH,
-            Movement.WEST: Movement.EAST,
-            Movement.EAST: Movement.WEST,
-        }
-
-        new_direction = location_dict[current_direction.value]
-        return Movement(new_direction)
-
-DIRECTIONS = [Movement.NORTH, Movement.SOUTH, Movement.WEST, Movement.EAST]
-ANTI_DIRECTIONS = [Movement.SOUTH, Movement.NORTH, Movement.EAST, Movement.WEST]
-DELTAS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
 
 
 class StatusCode(IntEnum):
     HIT_WALL = 0
     MOVE_REQUESTED = 1
-    MOVE_REQUESTED_INSIDE = 2
+    OXYGEN = 2
 
 
 class Tile(IntEnum):
@@ -98,14 +57,11 @@ class Tile(IntEnum):
     def show(tile):
         values = {
             Tile.DROID: "D",
-            Tile.WALL: "#",
-            Tile.OPEN: ".",
+            Tile.WALL: "░",
+            Tile.OPEN: " ",
             Tile.OXYGEN: "*",
         }
-        if tile in values:
-            return values[tile]
-        else:
-            return " "
+        return values[tile]
 
 
 class Location:
@@ -123,20 +79,18 @@ class Location:
         return f"({self.point} - {tile}"
 
 
+class VisitQueue(NamedTuple):
+    computer_state: str
+    # direction: Union[bool, Direction]
+    point: Point
+    num_steps: int
+
+
 class Day15:
     grid: Dict[Point, Location]
-    paths: List[List[str]]
+    oxygen_points: set
 
-    def get_location(self, point: Point) -> Location:
-        """ Get a locatino from the grid, or create a new location """
-        if location := self.grid.get(point):
-            return location
-        else:
-            new_location = Location(point=point)
-            self.grid[point] = new_location
-            return new_location
-
-    def show_screen(self, droid_point: Point, direction: Union[bool, Movement]):
+    def show_screen(self, droid_point: Point):
         """
         Draw the current grid on the console:
         """
@@ -150,16 +104,7 @@ class Day15:
             for x in range(min_x, max_x + 1):
                 point = Point(x, y)
                 if y == droid_point.y and x == droid_point.x:
-                    if direction == Movement.NORTH:
-                        row.append("v")
-                    elif direction == Movement.EAST:
-                        row.append(">")
-                    elif direction == Movement.SOUTH:
-                        row.append("^")
-                    elif direction == Movement.WEST:
-                        row.append("<")
-                    else:
-                        row.append("D")
+                    row.append("D")
                 else:
                     if point in self.grid:
                         row.append(Tile.show(self.grid[point].location_type))
@@ -168,18 +113,16 @@ class Day15:
             output.append("".join(row))
         return "\n".join(output)
 
-    def create_grid(self):
-        self.grid = {}
-        self.paths = []
-
     def run_robot(self, input_data: str):
+        self.grid = {}
+        self.oxygen_points = set()
+
         visited = {Point(0, 0)}
 
-        inputs = deque([])
+        input_direction = 0
 
         def get_input() -> int:
-            res = inputs.popleft()
-            return res
+            return input_direction
 
         # computer = IntcodeComputer(PROGRAM, get_input)
         computer = IntCode()
@@ -188,37 +131,58 @@ class Day15:
         computer.set_input_function(get_input)
 
         frontier = deque([])
-        frontier.append((computer.save(), 0, Point(0, 0)))
+        # frontier will be a deque with VisitQueue
+        frontier.append(
+            VisitQueue(computer_state=computer.save(), point=Point(0, 0), num_steps=0,)
+        )
 
         while frontier:
-            save_state, num_steps, point = frontier.popleft()
-            computer.load(save_state)
-            for direction, anti, (dx, dy) in zip(DIRECTIONS, ANTI_DIRECTIONS, DELTAS):
-                inputs.append(direction.value)
+            queue = frontier.popleft()
+
+            for direction in Direction:
+                computer.load(queue.computer_state)
+                input_direction = direction.value
                 status = StatusCode(computer.run_return_or_raise())
-                if status == StatusCode.MOVE_REQUESTED_INSIDE:
-                    print("found oxygen after", num_steps + 1, "steps")
-                    return num_steps + 1, computer
+                if status == StatusCode.OXYGEN:
+                    self.grid[queue.point] = Location(
+                        point=queue.point, location_type=Tile.OXYGEN
+                    )
+
+                    self.oxygen_points.add(
+                        (self.grid[queue.point], queue.num_steps + 1)
+                    )
                 elif status == StatusCode.HIT_WALL:
-                    pass
+                    new_loc = Direction.direction_update_x_y(
+                        current_direction=direction, point=queue.point
+                    )
+                    self.grid[new_loc] = Location(
+                        point=new_loc, location_type=Tile.WALL
+                    )
+
                 elif status == StatusCode.MOVE_REQUESTED:
-                    new_loc = Point(point.x + dx, point.y + dy)
+                    new_loc = Direction.direction_update_x_y(
+                        current_direction=direction, point=queue.point
+                    )
                     if new_loc not in visited:
+                        # We only add a location if we haven't been there before
                         visited.add(new_loc)
-                        frontier.append((computer.save(), num_steps + 1, new_loc))
-                        inputs.append(anti.value)
-                        computer.run_return_or_raise()
-                    else:
-                        inputs.append(anti.value)
-                        computer.run_return_or_raise()
-        print("Nothing found?")
+                        frontier.append(
+                            VisitQueue(
+                                computer_state=computer.save(),
+                                point=new_loc,
+                                num_steps=queue.num_steps + 1,
+                            )
+                        )
+                        self.grid[queue.point] = Location(
+                            point=queue.point, location_type=Tile.OPEN
+                        )
 
 
 class Day15PartA(Day15, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        self.create_grid()
-        steps, _ = self.run_robot(input_data)
-        return steps
+        self.run_robot(input_data)
+        oxygen = self.oxygen_points.pop()
+        return oxygen[1]
 
 
 class Day15PartB(Day15, FileReaderSolution):
